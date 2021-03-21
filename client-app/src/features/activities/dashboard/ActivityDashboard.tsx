cdac540d91dca50c27943ff111540020285f5178
Dave Cameron <dave@tinyweb.ca>
2021-03-21 09:56:11 -0700

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard(
@@ -27,7 +28,8 @@ export default function ActivityDashboard(
         openForm, 
         closeForm,
         createOrEdit,
-        deleteActivity
+        deleteActivity,
+        submitting
     }: Props
     ) {
     return (
@@ -37,6 +39,7 @@ export default function ActivityDashboard(
                     activites={activities} 
                     selectActivity={selectActivity} 
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -47,7 +50,12 @@ export default function ActivityDashboard(
                     openForm={openForm} 
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit} 
+                    submitting={submitting} 
+                />}
             </Grid.Column>
         </Grid>
     )
