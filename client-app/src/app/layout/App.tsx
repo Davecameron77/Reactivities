7c1739c526e72056738328b2863405b61cc3942f
Dave Cameron <dave@tinyweb.ca>
2021-04-21 21:50:30 -0700

    End of section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,36 @@
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetail from '../../features/activities/details/ActivityDetail';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
 
   return (
     <Fragment>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage} />
       <Route  path={'/(.+)'} render={() => (
         <Fragment>
           <NavBar />
           <Container style={{ marginTop: '7em' }}>
-            <Route exact path='/activities' component={ActivityDashboard} />
-            <Route path='/activities/:id' component={ActivityDetail} />
-            <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            <Switch>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetail} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <Route path='/errors' component={TestErrors} />
+              <Route path='/server-error' component={ServerError} />
+              <Route component={NotFound} />
+            </Switch>
           </Container>
         </Fragment>
       )}/>
